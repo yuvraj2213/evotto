d952d587b8dabfac80c911d1b2c8d77d6878990d
Yuvraj Preet <yuvraj13preet@gmail.com>
2024-12-20 21:29:47 +0530

    Initialized Responsiveness Work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+
+  const [showMenu,setShowMenu]=useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo-img">
         <img src="/images/logo2.png" width={50} alt="Logo" />
         <div className="logo">Evotto</div>
       </div>
-      <ul className="nav-links">
+
+      <ul className={showMenu?'menu-nav-links':'nav-links'}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -25,9 +30,18 @@ const Navbar = () => {
         <li>
           <Link to="/contact">Contact Us</Link>
         </li>
+        <button className="ham-btn" onClick={()=>setShowMenu(!showMenu)}>
+        <GiHamburgerMenu />
+      </button>
       </ul>
+
+      <div className="hamburger-menu">
+      <button className="ham-btn" onClick={()=>setShowMenu(!showMenu)}>
+        <GiHamburgerMenu />
+      </button>
+      </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navbar; 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Home.css';
+import '../App.css'
 import HeroSection from '../components/HeroSection';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
