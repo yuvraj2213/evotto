3ebe4403815914fe04d6f90fa2993dcabb0e881e
Yuvraj Preet <yuvraj13preet@gmail.com>
2024-12-21 21:27:03 +0530

    Small Change

--- src/pages/Rental.jsx
+++ src/pages/Rental.jsx
@@ -4,12 +4,12 @@ import Footer from "../components/Footer";
 import RentalHeader from "../components/RentalHeader";
 import TripPlanner from "../components/TripPlanner";
 import CarCard from "../components/CarCard";
+import SocialLinks from '../components/SocialLinks';
 
 import "../App.css";
 import "../styles/Rental.css";
 
 const Rental = () => {
-
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearchChange = (e) => {
@@ -20,16 +20,18 @@ const Rental = () => {
     <>
       <UserNavbar />
       <section className="rental-header-main">
-        <RentalHeader handleSearchChange={handleSearchChange}/>
+        <RentalHeader handleSearchChange={handleSearchChange} />
       </section>
       <div className="trip-planner-main">
         <TripPlanner />
       </div>
 
       <div className="car-card-main">
-        <CarCard searchQuery={searchQuery}/>
+        <CarCard searchQuery={searchQuery} />
       </div>
-
+      <section className="social-links">
+        <SocialLinks />
+      </section>
       <Footer />
     </>
   );
