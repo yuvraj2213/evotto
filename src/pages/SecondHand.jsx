5559a66015ece6d3aa87b03b22d3ef48440be3d7
Yuvraj Preet <yuvraj13preet@gmail.com>
2024-12-22 23:23:42 +0530

    Added components in Second Hand car page

--- src/pages/SecondHand.jsx
+++ src/pages/SecondHand.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import "../styles/SecondHand.css";
 
 const SecondHand = () => {
   return (
-    <div>
-      Second Hand
+    <div className="page-container">
+      <Navbar />
+      <div className="content">
+        <div className="header">
+          <h2>I WANT TO</h2>
+        </div>
+        <div className="buttons">
+          <button className="buy-btn">BUY</button>
+          <button className="sell-btn">SELL</button>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default SecondHand
+export default SecondHand;
