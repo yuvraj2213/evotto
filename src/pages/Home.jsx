547890d6ec47e8dbb394c5211db26951e9b3b3fb
Yuvraj Preet <yuvraj13preet@gmail.com>
2024-12-22 14:11:29 +0530

    Slideshow Style Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import '../styles/Home.css';
-import '../App.css'
+import React from "react";
+import "../styles/Home.css";
+import "../App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import HeroSection from '../components/HeroSection';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Services from '../components/Services';
-import Chatbox from '../components/Chatbox';
-import SocialLinks from '../components/SocialLinks';
-import Slideshow from '../components/Slideshow';
-
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Services from "../components/Services";
+import Chatbox from "../components/Chatbox";
+import SocialLinks from "../components/SocialLinks";
+import Slideshow from "../components/Slideshow";
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
-    <div className="home">
-      <header className="hero-section">
-        <HeroSection/>
-      </header>
-      <section className="slideshow">
-        <Slideshow/>
-      </section>
-      <section className="services">
-        <Services/>
-      </section>
-      <section className='social-links'>
-      <SocialLinks/>
-      </section>
-      <Chatbox/>
-    </div>
-    <Footer/>
+      <Navbar />
+      <div className="home">
+        <header className="hero-section">
+          <HeroSection />
+        </header>
+        <section className="slideshow">
+          <Slideshow />
+        </section>
+        <section className="services">
+          <Services />
+        </section>
+        <section className="social-links">
+          <SocialLinks />
+        </section>
+        <Chatbox />
+      </div>
+      <Footer />
     </>
   );
 };
