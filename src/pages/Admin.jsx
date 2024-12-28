525e9378bf61e9f0bfae661e31979302e9fa5b8e
Yuvraj Preet <yuvraj13preet@gmail.com>
2024-12-28 15:07:34 +0530

    Added admin panel button

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,31 +1,38 @@
 import React, { useEffect } from "react";
 import AdminNavbar from "../components/AdminNavbar";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import "../styles/Admin.css";
 import Footer from "../components/Footer";
 import { useAuth } from "../store/auth";
 
 const Admin = () => {
   const { user, isLoading } = useAuth();
+  const location = useLocation(); // Hook to track location changes
 
   useEffect(() => {
+    // Set background for admin page
     if (user?.userData?.isAdmin) {
       document.body.style.backgroundColor = "black";
       document.body.style.backgroundImage = "none";
+    } else {
+      document.body.style.backgroundImage = '/images/background2.jpg'; // Default background
     }
 
+    // Cleanup on component unmount or when location changes
     return () => {
-      document.body.style.backgroundColor = "";
+      if (!user?.userData?.isAdmin) {
+        document.body.style.backgroundColor = "";
+        document.body.style.backgroundImage = '/images/background2.jpg'; // Restore image
+      }
     };
-  }, [user]);
+  }, [user, location]); // Trigger effect when `user` or `location` changes
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   if (!user?.userData?.isAdmin) {
-    return (
-    <Navigate to="/" />);
+    return <Navigate to="/" />;
   }
 
   console.log("Admin Status:", user?.userData?.isAdmin);
