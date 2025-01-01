a1e172a9427fb799b8bbb118f43de7626e3cc2d2
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-01-01 10:48:28 +0530

    Added get to home button in admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import AdminNavbar from "../components/AdminNavbar";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Link, Navigate, Outlet, useLocation } from "react-router-dom";
 import "../styles/Admin.css";
 import Footer from "../components/Footer";
 import { useAuth } from "../store/auth";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const Admin = () => {
   const { user, isLoading } = useAuth();
@@ -32,6 +33,10 @@ const Admin = () => {
 
   return (
     <>
+      <Link to="/" className="back-to-home-btn">
+        <FaArrowAltCircleLeft /> Back To Home Page
+      </Link>
+
       <div className="admin-layout">
         <AdminNavbar />
         <div className="admin-content">
