edb9289d5c28c1f3fe446b994f901b42b95122bf
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-01-24 21:57:34 +0530

    Certificate size changed

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -6,8 +6,12 @@ const Achievement = () => {
     <>
       <div className="inner-certificate">
         <h1 className="certificate-header">Our Achievments</h1>
-        <div className="certificate1"></div>
-        <div className="certificate2"></div>
+        <div className="certificate1">
+          <img src="/images/Certificate/Certificate_1.jpg" alt="" />
+        </div>
+        <div className="certificate2">
+          <img src="/images/Certificate/Certificate_2.jpg" alt=""/>
+        </div>
       </div>
     </>
   );
