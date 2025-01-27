8103fea64c76eac649ae9e67e1654db39f804f3a
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-01-27 22:27:27 +0530

    UI change

--- src/components/FormSwitcher.jsx
+++ src/components/FormSwitcher.jsx
@@ -108,3 +108,8 @@ const FormSwitcher = () => {
 };
 
 export default FormSwitcher;
+
+
+// https://www.google.com/maps/place/THE+BIKER+LANE+%7C+BIKE+RENTALS/@20.3631819,85.8302744,19.11z/data=!4m6!3m5!1s0x3a19a762d2f8df83:0xe0af579a4d135a8a!8m2!3d20.3632039!4d85.8306415!16s%2Fg%2F11l6gz4nrh?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D
+
+// https://www.google.com/maps/search/chai+lover+iter/@20.2510375,85.8000228,18.55z?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D
